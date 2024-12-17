81dee2e96972ebfd79b2608bc08dd3a73aeb5f9a
sap1ks <sap1kcsgo@gmail.com>
2024-12-17 11:06:50 +0200

    №2

--- Program.cs
+++ Program.cs
@@ -11,49 +11,21 @@ namespace prac17
     {
         static void Main(string[] args)
         {
-            //List<string> people = new List<string>(10);
-            //Output(people);
-            //List<string> strings = new List<string>() {"Маша", "Паша", "Саша"};
-            //Output(strings);
-            //var people2 = new List<string>() { "Cаша", "Маша", "Саша" };
-            //Output(people2);
-            //var employer = new List<string>(people2);
-            //Output(employer);
-            List<string> list = new List<string>(6);
-            list.Add("Война и Мир");
-            list.Add("Война и Мир");
-            list.Add("Саша");
-            list.Add("Война и Мир");
-            list.Add("Война и Мир");
-            list.Add("Война и Мир");
+            List<int> list = new List<int>() {20,15,13,145,15,113,14,15,16,16,113,
+                12,56,67,13,24,45,243,35,89};
+            Random rng = new Random();
+            int index = rng.Next(0,19);
+            list.Remove(index);
+            Console.WriteLine($"Удален {index} элемент списка");
             Output(list);
-            ToUp(list);
-            Console.WriteLine(list.Count());
-            list.Add("Колобок");
-            Output(list);
-            list.Insert(0, "Маша");
-            Output(list);
-            list.RemoveAt(2);
-            Output(list);
-            list.Reverse();
-            Output(list);
-            Console.ReadLine();
+            Console.ReadKey();
         }
-        static void Output(List<string> list)
+        static void Output(List<int> list)
         {
-            foreach(var el in list)
+            foreach (int item in list)
             {
-                Console.WriteLine(el);
+                Console.WriteLine(item);
             }
-            Console.WriteLine();
-        }
-        static void ToUp(List<string> list)
-        {
-            foreach (var el in list)
-            {
-                Console.WriteLine(el.ToUpper());
-            }
-            Console.WriteLine();
         }
     }
 }
